04b115c4894889a7165d80af8b54ac7647633ffe
Gagogaa <gregoryjmann26@gmail.com>
2017-01-12 12:39:55 -0500

    done with project 1

--- project1/app.js
+++ project1/app.js
@@ -1,17 +1,21 @@
-/*
-	Gregory Mann
-	people.emich.edu/gmann1
-	COSC 436
-	Assignment 01/05
-	WINTER 2017
-	This project searches and replaces strings of text within a text area.
-	It loads a specified UTF-8 file from the users machine to do the searching. 
-*/
+/**
+ * Gregory Mann
+ * people.emich.edu/gmann1
+ * COSC 436
+ * Assignment 01/05
+ * WINTER 2017
+ * This project searches and replaces strings of text within a text area.
+ * It loads a specified UTF-8 file from the users machine to do the searching.
+ */
 
-// search replace replace clauses replace to make a duplicate of the file and append it to the end
-// TODO make readfile part of the search_replace object
 function init(){
-	window.search_replace = (function () { // search_replace is a global object
+
+	/**
+	 * Search_replace is an object that contains methods for loading a file
+	 * into a text area, searching it's contents for a string, and replacing
+	 * the matched string with another string.
+	 */
+	window.search_replace = (function () { // Search_replace is a global object
 		var textArea = document.querySelector("textArea"),
 				pointer = -1,
 				searchText = textArea.value,
@@ -19,28 +23,74 @@ function init(){
 				matches = [],
 				obj = {};
 
-		// getMatches takes a body of text and a search term (baseStr, searchStr)
-		// Returns an array consisting of every matches starting index
+		/**
+		 * Returns an array containing the starting index of
+		 * each instance of searchStr in baseStr.
+		 *
+		 * @param {String} baseStr
+		 * @param {String} searchStr
+		 */
 		function getMatches(baseStr, searchStr){
 			var indexes = [],
-					nextIndex = 0;
+				nextIndex = 0;
 
 			for(var i = 0; /* nothing */; i++){
 				nextIndex = baseStr.indexOf(searchStr, nextIndex);
 
-				if(nextIndex === -1) // no match found 
+				if(nextIndex === -1) // No match found
 					break;
-				
+
 				indexes[i] = nextIndex;
 				nextIndex++;
 			}
 			return indexes;
 		}
 
-		// replace replaces the currently highlighted search result with the replace term
+
+		/**
+		 * Reads a UTF-8 file from an input[type = file] element
+		 * and assigns the value of the text to the textarea.
+		 *
+		 * With help from this link: http://www.alecjacobson.com/weblog/?p=1645
+		 */
+		obj.readFile = function(){
+			var file = document.getElementById("fileInput").files[0];
+
+			if (file) {
+				var reader = new FileReader();
+
+				reader.readAsText(file, "UTF-8");
+
+				reader.onload = function (event) {
+					var text = event.target.result;
+
+					document.getElementById("textArea").value = text;
+					searchText = textArea.value;
+					pointer = -1;
+
+					if(searchTerm == ""){
+						matches = [];
+					}else{
+						matches = getMatches(searchText, searchTerm);
+					}
+					obj.search();
+				} //END: onload
+			}
+		}; // END: readFile
+
+		/**
+		 * Replaces the currently highlighter search result with
+		 * the value in the input[id="replaceParam"] element.
+		 */
 		obj.replace = function (){
-			if(matches.length > 0){
-				searchText = textArea.value;				
+			var newTerm = document.getElementById("searchParam").value;
+
+			if (newTerm !== searchTerm) // Don't replace if search term has changed since last search
+				return;
+
+			if(matches.length !== 0){
+				searchText = textArea.value;
+
 				var newWord = document.getElementById("replaceParam").value,
 						startStr = searchText.substring(0, matches[pointer]),
 						replaceText= searchText.substring(matches[pointer]),
@@ -51,26 +101,30 @@ function init(){
 				finalText = startStr + replaceText;
 				searchText = finalText;
 				textArea.value = finalText;
-				
+
 				searchText = textArea.value;
 				matches = getMatches(searchText, searchTerm);
 
-				if(pointer > 0)
-					pointer = pointer - 1; // decrement pointer so search() doesn't accidently skip a match
+				if(pointer !== -1)
+					pointer = pointer - 1; // Decrement pointer so search() doesn't accidentally skip a match
 
 				obj.search();
 			}
 		};
 
-		// search will highlight matches one at a time in sequential order 
+		/**
+		 * Search will highlight each instance of the value in
+		 * input[id="searchParam"] one at a time in sequential
+		 * order within the textarea.
+		 */
 		obj.search = function (){
 			var newSearch = document.getElementById("searchParam").value;
 			searchText = textArea.value;
-			
+
 			if(searchTerm !== newSearch){ // If the word in the search box has changed
 				pointer = -1;
 				searchTerm = newSearch;
-				
+
 				if(searchTerm == ""){
 					matches = [];
 				}else{
@@ -78,31 +132,36 @@ function init(){
 				}
 			}
 
-			if(pointer === matches.length - 1) // wrap around after highlighting the last match
+			if(pointer === matches.length - 1) // Wrap around after highlighting the last match
 				pointer = -1;
-			
+
 			pointer = pointer + 1;
+
 			textArea.focus();
+			scrollTo(textArea, matches[pointer]);
 			textArea.setSelectionRange(matches[pointer], matches[pointer] + searchTerm.length);
-		}
+		};
 
 		return obj;
 	})(); // END: search_replace
-} // END: init
 
-// Read file pickup from http://www.alecjacobson.com/weblog/?p=1645
-function readFile(){
-	var file = document.getElementById("fileInput").files[0];
+	// Link: google chrome scroll fix from: http://stackoverflow.com/questions/7464282/javascript-scroll-to-selection-after-using-textarea-setselectionrange-in-chrome#7464299
 
-	if (file) {
-		var reader = new FileReader();
+	/**
+	 * Scroll textarea to position.
+	 *
+	 * @param {HTMLInputElement} textarea
+	 * @param {Number} position
+	 */
+	function scrollTo(textarea, position) {
+		if (!textarea) { return; }
+		if (position < 0) { return; }
 
-		reader.readAsText(file, "UTF-8");
-
-		reader.onload = function (event) {
-			var text = event.target.result;
-			document.getElementById("textArea").value = text;
-			document.getElementById('filenput').disabled = true;
+		var body = textarea.value;
+		if (body) {
+			textarea.value = body.substring(0, position);
+			textarea.scrollTop = position;
+			textarea.value = body;
 		}
 	}
-}
+} // END: init
