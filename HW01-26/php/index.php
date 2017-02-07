379685061df5807ec50dbe93af2e1d4cb03bd25f
Gagogaa <gregoryjmann26@gmail.com>
2017-02-07 14:51:33 -0500

    did homework

--- HW01-26/php/index.php
+++ HW01-26/php/index.php
@@ -1,65 +1,74 @@
+<!--
+		 Gregory Mann
+		 Homework 01-26
+	 -->
+
 <!doctype html>
 <html lang="en">
 	<head>
-	<meta charset="UTF-8"/>
-	<title>Document</title>
+		<meta charset="UTF-8"/>
+		<title>Document</title>
 	</head>
 	<body>
 		<form action="index.php" method="POST">
 
-		<?php
-		class data {
-		  function __construct($id, $item, $cost) {
-				$this->id = $id;
-				$this->item = $item;
-				$this->cost = $cost;
-			}
-		}
-
-		$dataSet = array(
-			new data(20, "Pen", 2),
-			new data(55, "Pencil", 1),
-			new data(57, "Paper", 30),
-			new data(60, "Stapler", 50)
-		);
-
-		$amountOfItems = array();
-		
-		if(count($_POST) != 0){ // if i have recived form data
-			$total = 0;
-			echo '<table><tr><td>Item</td><td>Quantity</td><td>Total Cost</td>';
-			
-			for ($i = 0; $i < count($_POST); $i++){
-				if($_POST[$_dataSet[$i]->item] == 0){
-					
-				} else {
-					$total += $_POST[$dataSet[$i]->item] * $dataSet[$i]->cost;
-					echo '<tr><td>' . $dataSet[$i]->item .
-							 '</td><td>' . $_POST[$dataSet[$i]->item] .
-							 '</td><td>' . ($_POST[$dataSet[$i]->item] * $dataSet[$i]->cost) .
-							 '</td></tr>';
-					
-
+			<?php
+			/* create the data records */			
+			class data {
+				function __construct($id, $item, $cost) {
+					$this->id = $id;
+					$this->item = $item;
+					$this->cost = $cost;
 				}
 			}
-			echo "</table><p>Total Order Cost: " + $total + "</p>";
-			
-		} else {
-			echo '<table><tr><td>ID</td><td>Item</td><td>Cost per item</td><td>Quantity Ordered</td></tr>';
-			for ($i = 0; $i < count($dataSet); $i++){
-				echo '<tr><td>' . $dataSet[$i]->id . '</td>' .
-						 '<td>' . $dataSet[$i]->item . '</td>' .
-						 '<td>' . $dataSet[$i]->cost . '</td>' .
-						 '<td><label><input name="' . $dataSet[$i]->item .
-						 '" id="' . $dataSet[$i]->id .
-						 '" type="number" value="0"/></label></td></tr>';
-			}
-			echo "</table>";
-		}
 
-		
-		?>
-			<button>Calculate order</button>
+			/* fill the array with the data records */
+			$dataSet = array(
+				new data(20, "Pen", 2),
+				new data(55, "Pencil", 1),
+				new data(57, "Paper", 30),
+				new data(60, "Stapler", 50)
+			);
+
+			/* determine if the page was loaded with data and print accordingly */
+			if(count($_POST) != 0){ // if i have recived form data
+				$total = 0;
+				echo '<table><tr><td>Item</td><td>Quantity</td><td>Total Cost</td>';
+
+				/* construct the table elements if the number of items ordered is > 0 */
+				for ($i = 0; $i < count($_POST); $i++){
+					if($_POST[$dataSet[$i]->item] == 0){
+						
+					} else {
+						
+						$total += $_POST[$dataSet[$i]->item] * $dataSet[$i]->cost;
+						
+						echo '<tr><td>' . $dataSet[$i]->item .
+								 '</td><td>' . $_POST[$dataSet[$i]->item] .
+								 '</td><td>' . ($_POST[$dataSet[$i]->item] * $dataSet[$i]->cost) .
+								 '</td></tr>';
+						
+
+					}
+				}
+				echo "</table><p>Total Order Cost: " . $total . "</p>";
+				
+			} else { /* I havnt recived form data */
+				echo '<table><tr><td>ID</td><td>Item</td><td>Cost per item</td><td>Quantity Ordered</td></tr>';
+
+				/* construct the table data from the dataSet */
+				for ($i = 0; $i < count($dataSet); $i++){
+					echo '<tr><td>' . $dataSet[$i]->id . '</td>' .
+							 '<td>' . $dataSet[$i]->item . '</td>' .
+							 '<td>' . $dataSet[$i]->cost . '</td>' .
+							 '<td><label><input name="' . $dataSet[$i]->item .
+							 '" id="' . $dataSet[$i]->id .
+							 '" type="number" value="0"/></label></td></tr>';
+				}
+				echo "</table><button>Calculate order</button>";
+			}
+			?>
+
 		</form>
 	</body>
 </html>
